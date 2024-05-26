abc009b4c164d62ce10ca8967c875bee650df116
unknown <you@example.com>
2024-05-26 16:46:24 +0500

    added frontend and adjusted price in account of inflation

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import pickle
 import pandas as pd
 import numpy as np
+import streamlit as st
 
+st.set_page_config(page_title="Music Recommendation System")
 
 try:
     with open('finalized_model.pkl', 'rb') as model_file:
         model = pickle.load(model_file)
 except Exception as e:
     print(f"Error loading pickle file: {e}")
-
 
 features = pd.read_csv('features.csv')
 
@@ -21,9 +22,27 @@ def prediction(location,sqft,bedrooms,baths):
     x[2] = bedrooms
     if loc_index >= 0:
         x[loc_index] = 1
-    return model.predict([x])[0] / 100000
+      
+    result = model.predict([x])[0] / 1000000
+    print(result)
+    inflation_rate = 19.87
+    multiplier = 1 + (inflation_rate / 100)
+    adjusted_price = result * multiplier
 
-print(str(int(prediction('Nazimabad', 1800, 4, 3))) + " Lakhs")
+    return adjusted_price
+
+st.title("Karachi House Price Prediction")
+st.write("This is a simple web app to predict house prices in Karachi, Pakistan.")
+
+location = st.selectbox("Select Location", features.columns[3:])
+sqft = st.number_input("Square Feet")
+bedrooms = st.slider("Bedrooms" , max_value=8)
+baths = st.slider("Bathrooms", max_value=6)
+
+if st.button("Predict Price"):
+    price = str(int(prediction(location, sqft, bedrooms, baths)))
+    st.write(f"The estimated price of the house is: {price} Million PKR")
+
 
 
 
